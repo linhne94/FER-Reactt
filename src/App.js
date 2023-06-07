f93e38f23c48eb87a7e75c58e746b608d7efa04f
linhne94 <linhnqde160572@fpt.edu.vn>
2023-06-07 10:31:21 +0700

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import React,{Component} from 'react';
+import Menu from './components/MenuComponent';
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+        <Menu />
       </div>
     );
   }
